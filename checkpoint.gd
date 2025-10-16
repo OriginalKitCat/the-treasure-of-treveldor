5cbfca87cffdbe5f7b6f3d198f975f9bbd8fb484
KitCat <originalkitcat@proton.me>
2025-10-16 23:36:53 +0200

    made the checkpoint a laterna going on if activated

--- checkpoint.gd
+++ checkpoint.gd
@@ -4,8 +4,16 @@ extends Area2D
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
+	set_lantern_off()
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		checkpoint_manager.last_location = $Marker2D.global_position
+		set_lantern_on()
+
+func set_lantern_off():
+	$Laterna_image.texture = load("res://images/laterna_off.PNG")
+
+func set_lantern_on():
+	$Laterna_image.texture = load("res://images/laterna_on.PNG")
